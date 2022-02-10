2cda4f9813c2e60b092b98836e119a8b11ae0051
umar ismail <99389022+IT21233630@users.noreply.github.com>
2022-02-10 08:25:26 +0000

    completed tutorial 1

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,20 @@
 #include <stdio.h>
 
 int main() {
+
+  int sub1,sub2;
+  float avg=0;
+
+  printf("enter subject 1:");
+  scanf("%d",&sub1);
+
+  printf("enter subject2:");
+  scanf("%d",&sub2);
+
+  avg =sub1+sub2/2;
+
+  printf("The average is %f\n",avg);
+  
   
   return 0;
 }
